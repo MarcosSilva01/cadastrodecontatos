4fb19eb6f6bfc2c4fe2d0082bf4ca8f0a9a1a5d4
marcos.fvsilva <marcos.fvsilva@CAS419289C14728.senacsp.edu.br>
2015-08-14 22:16:27 -0300

    Exercicio Dupla Atualizado

--- src/main/java/br/senac/tads/pi3/marcosfelipe/cadastrodecontatos/exe1.java
+++ src/main/java/br/senac/tads/pi3/marcosfelipe/cadastrodecontatos/exe1.java
@@ -5,12 +5,29 @@
  */
 package br.senac.tads.pi3.marcosfelipe.cadastrodecontatos;
 
+import java.util.Scanner;
+
 /**
  *
  * @author marcos.fvsilva
  */
 public class exe1 {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Cadastro de Contatos:\n"
+                + "Nome, Data de Nascimento, E-mail, Telefone\n\n");
+        System.out.print("Nome: ");
+        String nome = entrada.nextLine();
+        System.out.print("Data de nascimento: ex(14/08/2015");
+        String dataNascimento = entrada.nextLine();
+        System.out.print("E-mail: ");
+        String email = entrada.nextLine();
+        System.out.print("Telefone: ");
+        String telefone = entrada.nextLine();
+        
+        
+        
+        
         
     }
           
