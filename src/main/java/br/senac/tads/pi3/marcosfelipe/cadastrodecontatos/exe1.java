f46799d23ff102e8c8b0364f693189c9c2e48555
felipe <felipe@felipe-PC>
2015-08-21 15:01:37 -0300

    21/08 - 15:01

--- src/main/java/br/senac/tads/pi3/marcosfelipe/cadastrodecontatos/exe1.java
+++ src/main/java/br/senac/tads/pi3/marcosfelipe/cadastrodecontatos/exe1.java
@@ -6,29 +6,46 @@
 package br.senac.tads.pi3.marcosfelipe.cadastrodecontatos;
 
 import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  *
  * @author marcos.fvsilva
  */
 public class exe1 {
+
+    static Connection con = null;
+    static String nome, dataNascimento, email, telefone;
+
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         System.out.println("Cadastro de Contatos:\n"
                 + "Nome, Data de Nascimento, E-mail, Telefone\n\n");
         System.out.print("Nome: ");
-        String nome = entrada.nextLine();
+        nome = entrada.nextLine();
         System.out.print("Data de nascimento: ex(14/08/2015");
-        String dataNascimento = entrada.nextLine();
+        dataNascimento = entrada.nextLine();
         System.out.print("E-mail: ");
-        String email = entrada.nextLine();
+        email = entrada.nextLine();
         System.out.print("Telefone: ");
-        String telefone = entrada.nextLine();
-        
-        
-        
-        
-        
+        telefone = entrada.nextLine();
     }
-          
+
+    public boolean entradaDados() throws ClassNotFoundException, SQLException {
+        try {
+            Cadastro cad = new Cadastro();
+            cad.setNome(nome);
+            cad.setDataNascimento(dataNascimento);
+            cad.setTelefone(telefone);
+            cad.setEmail(email);
+            
+            cad.insertContatos();
+            return true;
+        } catch (NumberFormatException | ClassNotFoundException error) {
+            System.out.println(error);
+            return false;
+        }
+    }
+
 }
